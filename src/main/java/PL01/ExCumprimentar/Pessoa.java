e13c8ac24658d10a53e3a55f1a4187c0c0ed026b
Edgar Moreira <ersmoreira@gmail.com>
2022-03-09 21:40:42 +0000

    Continuacao do Ex Cumprimentar PL01

--- src/main/java/PL01/ExCumprimentar/Pessoa.java
+++ src/main/java/PL01/ExCumprimentar/Pessoa.java
@@ -6,6 +6,7 @@ public class Pessoa {
     private int idade;
 
     private static final String NOME_POR_OMISSAO = "sem nome";
+    private static final int IDADE_POR_OMISSAO = 0;
 
     public Pessoa(String nome) {
         setNome(nome);
@@ -13,6 +14,12 @@ public class Pessoa {
 
     public Pessoa() {
         setNome(NOME_POR_OMISSAO);
+        setIdade(IDADE_POR_OMISSAO);
+    }
+
+    public Pessoa(String nome, int idade) {
+        setNome(nome);
+        setIdade(idade);
     }
 
     public String getNome() {
@@ -23,8 +30,17 @@ public class Pessoa {
         this.nome = nome;
     }
 
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+
     public String toString() {
-        return String.format("O nome é: %s", getNome());
+        return String.format("O nome é %s e tenho %d anos", getNome(), getIdade());
     }
 
 }
